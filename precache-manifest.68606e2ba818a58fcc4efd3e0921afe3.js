46f332f54e00a3580e86e6945921b1562bc86f5b
Kristijan Zadro <kristijan,zadroo30@gmail.com>
2021-04-27 14:11:02 +0200

    Updates

---
+++ precache-manifest.68606e2ba818a58fcc4efd3e0921afe3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7336fc3f31c8e5c2652917dfa6b5f71d",
+    "url": "/GitHubManager-React-Redux/index.html"
+  },
+  {
+    "revision": "8fd19db2c8ee30880475",
+    "url": "/GitHubManager-React-Redux/static/css/main.fae039ef.chunk.css"
+  },
+  {
+    "revision": "8ab68156534033bd45fe",
+    "url": "/GitHubManager-React-Redux/static/js/2.0012246c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GitHubManager-React-Redux/static/js/2.0012246c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fd19db2c8ee30880475",
+    "url": "/GitHubManager-React-Redux/static/js/main.b4adc276.chunk.js"
+  },
+  {
+    "revision": "06a93b238eb7e2bd216d",
+    "url": "/GitHubManager-React-Redux/static/js/runtime-main.e96ec7ea.js"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/GitHubManager-React-Redux/static/media/loading-arrow.e9ea7394.gif"
+  }
+]);
